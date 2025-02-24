91c846f1d728dbe031fe8b8446b1b7b94f37a59d
KavGoat <86602648+KavGoat@users.noreply.github.com>
2025-02-24 09:58:32 +0000

    commit

--- app.py
+++ app.py
@@ -95,23 +95,6 @@ def get_round(date, rounds):
         all_rounds_data[name]=individuals_round(date, rounds, name, length)
     return all_rounds_data, holes, pars, current_course, current_layout, names
 
-def winner (rounds, names):
-    scores = []
-    for name in names:
-        scores += [rounds[name][-1]]
-    players = len(scores)
-    best_score = min(scores)
-    winners = []
-    for name in names:
-        if rounds[name][-1] == best_score:
-            winners += [name]
-    if len(winners) == 1:
-        return [players, winners[0]]
-    elif len(winners) == 2:
-        return [players, f"{winners[0]} and {winners[1]}"]
-    else:
-        return [players, ", ".join(winners[:-1]) + " and " + winners[-1]]
-
 def get_best(rounds, dates, names):
     pars = [int(x) for x in rounds[dates[0]][0][8:] if str(x) != 'nan']
     length = len(pars)
@@ -249,17 +232,46 @@ def display_round(all_rounds_data, date, holes, pars, current_course, current_la
             st.markdown(f"<p style='text-align: left; font-size: clamp(12px, 2vw, 16px);'>{formatted_date_time}</p>", unsafe_allow_html=True)
         else:
             pass
-        winner_data = winner(all_rounds_data, names)
-        if winner_data[0]>1:
-            st.markdown(f"<p style='text-align: left; font-size: clamp(12px, 2vw, 16px);'>👑&nbsp;&nbsp;{winner_data[1]}</p>", unsafe_allow_html=True)
+        names = sorted(
+            names, 
+            key=lambda name: (- (all_rounds_data[name][-2] - all_rounds_data[name][-1]), all_rounds_data[name][-1])
+        )
+        first_place_value = (
+            all_rounds_data[names[0]][-2] - all_rounds_data[names[0]][-1], 
+            all_rounds_data[names[0]][-1]
+        )
+        first_place_names = [
+            name for name in names 
+            if (all_rounds_data[name][-2] - all_rounds_data[name][-1], all_rounds_data[name][-1]) == first_place_value
+        ]
+        first_place_string = first_place_names[0] if len(first_place_names) == 1 else " and ".join(first_place_names) if len(first_place_names) == 2 else ", ".join(first_place_names[:-1]) + ", and " + first_place_names[-1]
+        if len(names)>1:
+            st.markdown(f"<p style='text-align: left; font-size: clamp(12px, 2vw, 16px);'>👑&nbsp;&nbsp;{first_place_string}</p>", unsafe_allow_html=True)
         cols = st.columns(len(names))
-        names = sorted(names, key=lambda name: (all_rounds_data[name][-1]))
+        current_max = 0
+        fillers = []
         for index, name in enumerate(names):
             with cols[index]:
                 player_data = all_rounds_data[name]
+                current_max =  max(player_data[1] - player_data[2], current_max)
+                if player_data[1] - player_data[2] < sum(pars):
+                    if player_data[1] - player_data[2] == current_max:
+                        filler_ting = " (P)"
+                    else:
+                        filler_ting = " (PP)"
+                else:
+                    filler_ting = ""
+                fillers += [filler_ting]
                 score_display = f"{'+' if player_data[2] > 0 else ''}{'E' if player_data[2] == 0 else player_data[2]} ({player_data[1]})"
-                st.markdown(f"<p style='text-align: center; font-size: clamp(13px, 2.5vw, 18px); font-weight: bold;'>{name}</p>", unsafe_allow_html=True)
+                st.markdown(f"<p style='text-align: center; font-size: clamp(13px, 2.5vw, 18px); font-weight: bold;'>{name}{filler_ting}</p>", unsafe_allow_html=True)
                 st.markdown(f"<p style='text-align: center; font-size: clamp(12px, 2vw, 16px)'>{score_display}</p>", unsafe_allow_html=True)
+        if fillers != [""] * len(cols):
+            htmlting = f"<div style='line-height:0.75; padding-bottom:5px'><p style='text-decoration: underline; text-align: left; font-size: clamp(12px, 2vw, 16px)'>Notes</p>"
+            if " (P)" in fillers:
+                htmlting += f"<p style='text-align: left; font-size: clamp(12px, 2vw, 16px)'>(P) - Partial round</p>"
+            elif " (PP)" in fillers:
+                htmlting += f"<p style='text-align: left; font-size: clamp(12px, 2vw, 16px)'>(PP) - Fewer holes played than others</p>"
+            st.markdown(htmlting+"</div>", unsafe_allow_html=True)
         if date == "NA":
             package = st.container(border=True)
         else:
@@ -270,12 +282,8 @@ def display_round(all_rounds_data, date, holes, pars, current_course, current_la
                 full_table.append([player] + all_rounds_data[player][0])
             df = pd.DataFrame(full_table)
             html_string = "<div>"
-            chunk_size = 27
+            chunk_size = 18
             num_chunks = len(holes) // chunk_size + (1 if len(holes) % chunk_size != 0 else 0)
-            if len(holes) > 21:
-                fontsize=13
-            else:
-                fontsize=14
             for i in range(num_chunks):
                 start = i * chunk_size
                 end = min((i + 1) * chunk_size, len(holes))
@@ -285,7 +293,7 @@ def display_round(all_rounds_data, date, holes, pars, current_course, current_la
                 for player in names:
                     table.append([player] + all_rounds_data[player][0][start:end])
                 df = pd.DataFrame(table)
-                html_string += style_table(df, f"calc(min(587px/{max(len(current_holes),1)},638px/{max(len(current_holes)+1,1)}))", "laptop-table",fontsize)
+                html_string += style_table(df, f"calc(min(587px/{max(min(len(holes), chunk_size),1)},638px/{max(min(len(holes), chunk_size)+1,1)}))", "laptop-table",14)
                 if i+1 < num_chunks:
                     html_string += '<div class="laptop-table"><hr style="margin-top:1em; margin-bottom:1em"></div>'
             chunk_size = 9
@@ -303,6 +311,106 @@ def display_round(all_rounds_data, date, holes, pars, current_course, current_la
                 if i+1 < num_chunks:
                     html_string += '<div class="mobile-table"><hr style="margin-top:1em; margin-bottom:1em"></div>'
             st.markdown(html_string+"</div>",unsafe_allow_html=True)
+
+def best_per_hole(rounds, dates, names):
+    pars = [int(x) for x in rounds[dates[0]][0][8:] if str(x) != 'nan']
+    length = len(pars)
+    all_data = {}
+    for name in names:
+        all_data[name] = None
+        tries = []
+        for date in dates:
+            if name in [row[0] for row in rounds[date] if len(row) > 0]:
+                tries += [individuals_round(date, rounds, name, length)[0]]
+        bests = []
+        par_score = 0
+        for i, par in enumerate(pars):
+            hole_scores = []
+            for roundr in tries:
+                if roundr[i] != "-":
+                    hole_scores += [roundr[i]]
+            if len(hole_scores) == 0:
+                bests += ["-"]
+            else:
+                bests += [min(hole_scores)]
+                par_score += min(hole_scores) - par
+        total = sum(item for item in bests if isinstance(item, (int, float)))
+        all_data[name] = [bests, total, par_score]
+    return all_data
+
+def get_average(rounds, dates, names):
+    pars = [int(x) for x in rounds[dates[0]][0][8:] if str(x) != 'nan']
+    length = len(pars)
+    all_data = {}
+    for name in names:
+        all_data[name] = None
+        tries = []
+        for date in dates:
+            if name in [row[0] for row in rounds[date] if len(row) > 0]:
+                tries += [individuals_round(date, rounds, name, length)[0]]
+        avs = []
+        avsf = []
+        par_score = 0
+        for i, par in enumerate(pars):
+            hole_scores = []
+            for roundr in tries:
+                if roundr[i] != "-":
+                    hole_scores += [roundr[i]]
+            if len(hole_scores) == 0:
+                avs += ["-"]
+                avsf += ["-"]
+            else:
+                avs += [statistics.mean(hole_scores)]
+                avsf += [round(float(statistics.mean(hole_scores)),1)]
+                par_score += statistics.mean(hole_scores) - par
+        if avs.count("-") == length:
+            par_score = "-"
+            total = "-"
+        else:
+            total = sum(item for item in avs if isinstance(item, (int, float)))
+            if round(float(total),1).is_integer():
+                total = int(round(float(total),1))
+            else:
+                total = round(float(total),1)
+            if round(float(par_score),1).is_integer():
+                par_score = int(round(float(par_score),1))
+            else:
+                par_score = round(float(par_score),1)            
+        all_data[name] = [avsf, total,par_score]
+                
+    return all_data
+
+def get_best(rounds, dates, names):
+    pars = [int(x) for x in rounds[dates[0]][0][8:] if str(x) != 'nan']
+    length = len(pars)
+    all_data = {}
+    for name in names:
+        all_data[name] = None
+        tries = []
+        scores = []
+        totals = []
+        for date in dates:
+            if name in [row[0] for row in rounds[date] if len(row) > 0]:
+                tries += [individuals_round(date, rounds, name, length)[0]]
+                scores += [individuals_round(date, rounds, name, length)[2]]
+                totals += [individuals_round(date, rounds, name, length)[1]]
+        if totals.count("-") == len(totals):
+            all_data[name] = [["-"] * length, '-', "-"]
+        else:
+            filtered_scores = [a for a in scores if a != "-"]
+            filtered_totals = [a for a in totals if a != "-"]
+            max_diff = max([x - y for x, y in zip(filtered_totals, filtered_scores)])
+            valid_scores = []
+            for i in range(len(filtered_scores)):
+                if filtered_totals[i] - filtered_scores[i] == max_diff:
+                    valid_scores += [filtered_scores[i]]
+            min_score=min(valid_scores)
+            for i in range(len(tries)):
+                if scores[i] != "-":
+                    if totals[i] - scores[i] == max_diff and scores[i]==min_score:
+                        if all_data[name]==None:
+                            all_data[name] = [tries[i], totals[i], scores[i]]
+    return all_data
 
 def main():
     st.markdown("""
@@ -348,5 +456,19 @@ def main():
             for date in dates:
                 all_rounds_data, holes, pars, current_course, current_layout, round_names = get_round(date, rounds) 
                 display_round(all_rounds_data, date, holes, pars, current_course, current_layout, round_names)
+        elif Type == "Best Round":
+            all_rounds_data, holes, pars, current_course, current_layout,round_names = get_round(dates[0], rounds)
+            best_rounds = get_best(rounds, dates, names)
+            display_round(best_rounds, "NA", holes, pars, current_course, current_layout, names)
+        
+        elif Type == "Best Per Hole":
+            all_rounds_data, holes, pars, current_course, current_layout,round_names= get_round(dates[0], rounds)
+            best_rounds = best_per_hole(rounds, dates, names)
+            display_round(best_rounds, "NA", holes, pars, current_course, current_layout, names)
+        
+        elif Type == "Average":
+            all_rounds_data, holes, pars, current_course, current_layout,round_names = get_round(dates[0], rounds)
+            best_rounds = get_average(rounds, dates, names)
+            display_round(best_rounds, "NA", holes, pars, current_course, current_layout, names)
 
 main()
